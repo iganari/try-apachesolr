03f69e5006a4c6cd124f298dfb31be95e3b38984
cigaguri <cigaguri@gmail.com>
2016-03-31 19:42:31 +0900

    変数でまとめる

--- opsfiles/provision2.sh
+++ opsfiles/provision2.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 : "install Zookeeper" && {
+ZKR='zookeeper-3.4.8'
+
 cd /var/tmp
-wget http://ftp.meisei-u.ac.jp/mirror/apache/dist/zookeeper/zookeeper-3.4.8/zookeeper-3.4.8.tar.gz
-tar zxvf zookeeper-3.4.8.tar.gz
-ln -s /var/tmp/zookeeper-3.4.8 /var/lib/zookeeper
-cd /var/lib/zookeeper
+wget http://ftp.meisei-u.ac.jp/mirror/apache/dist/zookeeper/zookeeper-3.4.8/${ZKR}.tar.gz
+tar zxvf ${ZKR}.tar.gz
+cp -ar ${ZKR} /opt/
+ln /opt/${ZKR} /opt/zookeeper
+cd /opt
 cp -a ./conf/zoo{_sample,}.cfg
-cat /srv/try-apachesolr/zoo.cfg >> /var/lib/zookeeper/conf/zoo.cfg 
+cat /srv/try-apachesolr/zoo.cfg >> /opt/zookeeper/conf/zoo.cfg 
 ./bin/zkServer.sh start
 }
 
