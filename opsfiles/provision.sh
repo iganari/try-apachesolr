e5a30db754fe2926564a089120c0a6bb48fdbe06
cigaguri <cigaguri@gmail.com>
2016-03-31 11:13:28 +0900

    update

--- opsfiles/provision.sh
+++ opsfiles/provision.sh
@@ -23,6 +23,4 @@ VERSION='5.5.0'
 cd ${TMP_DIR}
 wget http://ftp.meisei-u.ac.jp/mirror/apache/dist/lucene/solr/${VERSION}/solr-${VERSION}.tgz
 tar zxvf ${TMP_DIR}/solr-${VERSION}.tgz
-sh solr-${VERSION}/bin/install_solr_service.sh solr-${VERSION}.tgz
-
-# curl http://localhost:8983/solr/admin/ | head -n30
+sh ${TMP_DIR}/solr-${VERSION}/bin/install_solr_service.sh solr-${VERSION}.tgz
