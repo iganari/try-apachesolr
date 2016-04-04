b85365717befd288dce6a6bd255884327d0c1a5c
iganari <cigaguri@gmail.com>
2016-04-04 19:48:08 +0900

    zoo.conf全体をgit管理下に置く

--- opsfiles/provision-zk.sh
+++ opsfiles/provision-zk.sh
@@ -4,24 +4,27 @@ st -xe
 
 : "install Zookeeper" && {
 ZKR='zookeeper-3.4.8'
+V_DIR='/srv/try-apachesolr'
+Z_DIR='/opt/zookeeper'
 
 cd /var/tmp
-wget http://ftp.meisei-u.ac.jp/mirror/apache/dist/zookeeper/zookeeper-3.4.8/${ZKR}.tar.gz
+wget http://ftp.meisei-u.ac.jp/mirror/apache/dist/zookeeper/${ZKR}/${ZKR}.tar.gz
 tar zxvf ${ZKR}.tar.gz
 cp -ar ${ZKR} /opt/
-ln -s /opt/${ZKR} /opt/zookeeper
-cd /opt/zookeeper
-cp -a ./conf/zoo{_sample,}.cfg
-cat /srv/try-apachesolr/zoo.cfg >> /opt/zookeeper/conf/zoo.cfg 
+ln -s /opt/${ZKR} ${Z_DIR}
+cd ${Z_DIR}
+# cp -a ./conf/zoo{_sample,}.cfg
+# cat /srv/try-apachesolr/zoo.cfg >> /opt/zookeeper/conf/zoo.cfg 
+ln -s ${V_DIR}/zoo.cfg ${Z_DIR}/conf/zoo.cfg
 ./bin/zkServer.sh start
 }
 
-: "setting Zookeeper" && {
+: "launch Zookeeper id" && {
 mkdir -p /tmp/zookeeper
 hostname | sed 's/svr0//g' > /tmp/zookeeper/myid
 }
 
 : "restart zk" && {
-cd /opt/zookeeper
+cd ${Z_DIR}
 ./bin/zkServer.sh restart
 }
